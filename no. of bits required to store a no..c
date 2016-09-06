7043ef740e2977a6bb07a6a68247204af358910a
piyushlionhearts <piyushlionhearts@gmail.com>
2016-09-06 21:39:09 +0530

    negative number correction

--- no. of bits required to store a no..c
+++ no. of bits required to store a no..c
@@ -3,17 +3,31 @@ this peice of code calculates the no. of bit required to
 be set to store a number in memory */
 
 #include <stdio.h>
-int main(){
+int main()
+{
     printf ("enter any number\n");
     int input ;
     scanf("%d",&input);
     int count =0 ;
 
-  //********** main part starts here
-
-    while (input != 0){
-       count ++;
-       input = input>>1;
+    //********** main part starts here
+    //***********for positive no.
+    if (input >= 0)
+    {
+        while (input != 0)
+        {
+            count ++;
+            input = input>>1;
+        }
+    }
+    //*********for negative no.
+    else
+    {
+        while (input != 0)
+        {
+            count ++;
+            input = input<<1;
+        }
     }
     //main part ends
     printf ("%d",count);
